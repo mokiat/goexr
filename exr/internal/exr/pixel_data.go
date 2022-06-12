d7dabea9c4a9c3d8636ae3d083945e797b71f4ed
Momchil Atanasov <momchil.dev@gmail.com>
2022-06-12 22:38:17 +0300

    Extend pixel data concept

--- exr/internal/exr/pixel_data.go
+++ exr/internal/exr/pixel_data.go
@@ -8,6 +8,7 @@ import (
 )
 
 type PixelData interface {
+	LineSize() int32
 	ReadLine(in io.Reader, y int32) error
 	Float32(x, y int) float32
 }
@@ -22,12 +23,41 @@ type nopPixelData struct {
 	value float32
 }
 
+func (d *nopPixelData) LineSize() int32 {
+	return 0
+}
+
 func (d *nopPixelData) ReadLine(in io.Reader, y int32) error {
 	return fmt.Errorf("cannot read into nop pixel data")
 }
 
 func (d *nopPixelData) Float32(x, y int) float32 {
 	return d.value
+}
+
+func NewUint32PixelData(window Box2i, xSampling, ySampling int32) PixelData {
+	return &uint32PixelData{
+		width: window.Width() / xSampling,
+	}
+}
+
+type uint32PixelData struct {
+	width int32
+}
+
+func (d *uint32PixelData) LineSize() int32 {
+	return d.width * 4
+}
+
+func (d *uint32PixelData) ReadLine(in io.Reader, y int32) error {
+	if _, err := io.CopyN(io.Discard, in, int64(d.width)*4); err != nil {
+		return fmt.Errorf("error reading uint32 pixel slice: %w", err)
+	}
+	return nil
+}
+
+func (d *uint32PixelData) Float32(x, y int) float32 {
+	return 0.0 // uint32 is used for object reference, not colors
 }
 
 func NewFloat16PixelData(window Box2i, xSampling, ySampling int32) PixelData {
@@ -48,12 +78,17 @@ type float16PixelData struct {
 	pixels    []float16.Float16
 }
 
+func (d *float16PixelData) LineSize() int32 {
+	width := d.window.Width() / d.xSampling
+	return width * 2
+}
+
 func (d *float16PixelData) ReadLine(in io.Reader, y int32) error {
 	width := d.window.Width() / d.xSampling
 	y = (y - d.window.YMin) / d.ySampling
 	offset := y * width
 	if err := Read(in, d.pixels[offset:offset+width:offset+width]); err != nil {
-		return fmt.Errorf("error reading pixel slice: %w", err)
+		return fmt.Errorf("error reading float16 pixel slice: %w", err)
 	}
 	return nil
 }
@@ -71,4 +106,44 @@ func (d *float16PixelData) Float32(x, y int) float32 {
 		value = float16.Frombits(uint16(0x0000)) // min value
 	}
 	return value.Float32()
+}
+
+func NewFloat32PixelData(window Box2i, xSampling, ySampling int32) PixelData {
+	width := window.Width() / xSampling
+	height := window.Height() / ySampling
+	return &float32PixelData{
+		window:    window,
+		xSampling: xSampling,
+		ySampling: ySampling,
+		pixels:    make([]float32, width*height),
+	}
+}
+
+type float32PixelData struct {
+	window    Box2i
+	xSampling int32
+	ySampling int32
+	pixels    []float32
+}
+
+func (d *float32PixelData) LineSize() int32 {
+	width := d.window.Width() / d.xSampling
+	return width * 4
+}
+
+func (d *float32PixelData) ReadLine(in io.Reader, y int32) error {
+	width := d.window.Width() / d.xSampling
+	y = (y - d.window.YMin) / d.ySampling
+	offset := y * width
+	if err := Read(in, d.pixels[offset:offset+width:offset+width]); err != nil {
+		return fmt.Errorf("error reading float32 pixel slice: %w", err)
+	}
+	return nil
+}
+
+func (d *float32PixelData) Float32(x, y int) float32 {
+	offX := (int32(x) - d.window.XMin) / d.xSampling
+	offY := (int32(y) - d.window.YMin) / d.ySampling
+	width := d.window.Width() / d.xSampling
+	return d.pixels[offX+width*offY]
 }
